fb37cd2387b28ff86fb5be8ba5f41d208686f134
Rob Orsini <rob.orsini@autodesk.com>
2019-08-22 20:31:15 -0700

    button test for phone

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import gotScales from 'got-scales'
 
 const App = () => {
 
-  console.log("gotScales:");
-  console.log(gotScales);
+  // console.log("gotScales:");
+  // console.log(gotScales);
 
   const [bassMode, setBassMode] = React.useState('');
   const [bassNote, setBassNote] = React.useState('C');
@@ -22,11 +22,9 @@ const App = () => {
     return s && s.substring(0, 2)
   }) : [];
 
-  console.log("scale");
-  console.log(scale);
-
-  console.log("displayStyle:");
-  console.log(displayStyle);
+  console.log("bassMode: " + bassMode);
+  console.log("scale: " + scale);
+  console.log("displayStyle: " + displayStyle);
 
   const PianoContainer = styled.div`
     padding: 0px 0px 0px 40px;
@@ -39,12 +37,53 @@ const App = () => {
     font-weight: bold;
   `;
 
+  const NoteButtonContainer = styled.div`
+    display: flexbox;
+  `;
+
+  const NoteButton = styled.button`
+    font-weight: bold;
+    font-size: 1.8em;
+    padding: 10p 10p 10p 10px;
+    margin: 20px;
+    width: 60px;
+    height: 60px;
+    background-color: #008CBA;
+    border-radius: 8px;
+    border-color: black;
+    flex: 1;
+  `;
+
+  const handleNoteChange = (note) => {
+    console.log("clicked: " + note);
+    setBassNote(note);
+    setBassMode('0,2,4,6');
+  };
+
+  console.log("bassNote2: " + bassNote);
+
   return (
     <div>
       <InstructionsContainer>
         Choose some options:
       </InstructionsContainer>
-      <BassOptionsMenu setBassNote={setBassNote} setBassMode={setBassMode} setDisplayStyle={setDisplayStyle}/>
+
+      <NoteButtonContainer>
+        <NoteButton onClick={(e) => {
+          handleNoteChange('C');
+        }}>C</NoteButton>
+        <NoteButton onClick={(e) => {
+          handleNoteChange('D');
+        }}>D</NoteButton>
+        <NoteButton onClick={(e) => {
+          handleNoteChange('E');
+        }}>E</NoteButton>
+        <NoteButton onClick={(e) => {
+          handleNoteChange('F');
+        }}>F</NoteButton>
+      </NoteButtonContainer>
+
+      {/*<BassOptionsMenu setBassNote={setBassNote} setBassMode={setBassMode} setDisplayStyle={setDisplayStyle}/>*/}
       <Bass scale={scale} display_style={displayStyle}/>
 
       <PianoContainer>
